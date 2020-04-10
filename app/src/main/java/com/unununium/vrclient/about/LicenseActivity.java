f2a6e3b13c54cfed1194f38bb9383ddb62df2c9b
PC Chin <chin.pengcheng@gmail.com>
2020-04-10 17:41:24 +0800

    Implemented LicenseView

--- app/src/main/java/com/unununium/vrclient/about/LicenseActivity.java
+++ app/src/main/java/com/unununium/vrclient/about/LicenseActivity.java
@@ -18,131 +18,29 @@
 
 package com.unununium.vrclient.about;
 
-import android.annotation.SuppressLint;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.unununium.vrclient.functions.FileFunctions;
-import com.unununium.vrclient.functions.UIFunctions;
+import com.pcchin.licenseview.LicenseView;
 import com.unununium.vrclient.R;
-import com.unununium.vrclient.view.AutoDismissDialog;
-
-import java.util.Arrays;
-import java.util.Objects;
 
 public class LicenseActivity extends AppCompatActivity {
     private static final int[] licenseArrays = new int[]{ R.array.study_assistant_license,
-            R.array.okhttp_license, R.array.socketio_client_java_license };
+            R.array.licenseview_license, R.array.okhttp_license, R.array.socketio_client_java_license };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_license);
 
-        @SuppressLint("InflateParams")
-        ScrollView returnScroll = (ScrollView) getLayoutInflater().inflate(R.layout.blank_list, null, false);
-        LinearLayout returnLayout = returnScroll.findViewById(R.id.blank_linear);
-
+        LicenseView licenseView = findViewById(R.id.license_view);
         for (int licenseArray : licenseArrays) {
             // Updates license info & OnClickListeners
             String[] infoArray = getResources().getStringArray(licenseArray);
-            if (infoArray.length == 3 && getFragmentManager() != null) {
-                @SuppressLint("InflateParams") LinearLayout licenseDisplay = (LinearLayout) getLayoutInflater()
-                        .inflate(R.layout.license_display, null);
-                // Common functions used by all licenses for neatness
-                ((TextView) licenseDisplay.findViewById(R.id.m4_lib)).setText(infoArray[1]);
-                DialogInterface.OnClickListener[] yListeners = new DialogInterface
-                        .OnClickListener[]{null, (dialogInterface, i1) ->
-                        dialogInterface.dismiss(), null};
-                String[] buttonList = {"", getString(R.string.close), ""};
-
-                if (Objects.equals(infoArray[0], getString(R.string.license_apache_2))) {
-                    // Apache 2.0 license
-                    ((TextView) licenseDisplay.findViewById(R.id.m4_type)).setText(R.string.license_apache_2);
-                    licenseDisplay.setOnClickListener(view -> {
-                        // TextView needs to be set for each instance to prevent error
-                        TextView licenseView = new TextView(this);
-                        licenseView.setTextSize(18);
-                        licenseView.setPadding(20, 20, 20, 20);
-                        UIFunctions.setHtml(licenseView,
-                                infoArray[2] + FileFunctions.getTxt(this,
-                                        "apache_2_license.txt"));
-                        new AutoDismissDialog(getString(R.string.license_apache_2), licenseView,
-                                buttonList, yListeners).show(getSupportFragmentManager(),
-                                "LicenseActivity.Apache2");
-                    });
-                } else if (Objects.equals(infoArray[0], getString(R.string.license_mit))) {
-                    // MIT license
-                    ((TextView) licenseDisplay.findViewById(R.id.m4_type)).setText(R.string.license_mit);
-                    licenseDisplay.setOnClickListener(view -> {
-                        // TextView needs to be set for each instance to prevent error
-                        TextView licenseView = new TextView(this);
-                        licenseView.setTextSize(18);
-                        licenseView.setPadding(20, 20, 20, 20);
-                        UIFunctions.setHtml(licenseView,
-                                infoArray[2] + FileFunctions.getTxt(this,
-                                        "mit_license.txt"));
-                        new AutoDismissDialog(getString(R.string.license_mit), licenseView,
-                                buttonList, yListeners).show(getSupportFragmentManager(),
-                                "LicenseActivity.MIT");
-                    });
-                } else if (Objects.equals(infoArray[0], getString(R.string.license_cc_3_unported))) {
-                    // Creative Commons CC 3.0 Unported
-                    ((TextView) licenseDisplay.findViewById(R.id.m4_type)).setText(R.string.license_cc_3_unported);
-                    licenseDisplay.setOnClickListener(view -> {
-                        // TextView needs to be set for each instance to prevent error
-                        TextView licenseView = new TextView(this);
-                        licenseView.setTextSize(18);
-                        licenseView.setPadding(20, 20, 20, 20);
-                        UIFunctions.setHtml(licenseView,
-                                infoArray[2] + FileFunctions.getTxt(this,
-                                        "cc_3_unported.txt"));
-                        new AutoDismissDialog(getString(R.string.license_cc_3_unported), licenseView,
-                                buttonList, yListeners).show(getSupportFragmentManager(),
-                                "LicenseActivity.CC3_Unported");
-                    });
-                } else if (Objects.equals(infoArray[0], getString(R.string.license_gnu_gpl_v3))) {
-                    // GNU GPL V3 License
-                    ((TextView) licenseDisplay.findViewById(R.id.m4_type)).setText(R.string.license_gnu_gpl_v3);
-                    licenseDisplay.setOnClickListener(view -> {
-                        // TextView needs to be set for each instance to prevent error
-                        TextView licenseView = new TextView(this);
-                        licenseView.setTextSize(18);
-                        licenseView.setPadding(20, 20, 20, 20);
-                        UIFunctions.setHtml(licenseView,
-                                infoArray[2] + FileFunctions.getTxt(this,
-                                        "gnu_gpl_v3_license.txt"));
-                        new AutoDismissDialog(getString(R.string.license_gnu_gpl_v3), licenseView,
-                                buttonList, yListeners).show(getSupportFragmentManager(),
-                                "LicenseActivity.GNU_GPL_V3");
-                    });
-                } else if (Objects.equals(infoArray[0], getString(R.string.license_none))) {
-                    // No license
-                    ((TextView) licenseDisplay.findViewById(R.id.m4_type)).setText(R.string.license_none);
-                    licenseDisplay.setOnClickListener(view -> {
-                        // TextView needs to be set for each instance to prevent error
-                        TextView licenseView = new TextView(this);
-                        licenseView.setTextSize(18);
-                        licenseView.setPadding(20, 20, 20, 20);
-                        UIFunctions.setHtml(licenseView, infoArray[2]);
-                        new AutoDismissDialog(getString(R.string.license_none), licenseView,
-                                buttonList, yListeners).show(getSupportFragmentManager(),
-                                "LicenseActivity.None");
-                    });
-                }
-                returnLayout.addView(licenseDisplay);
-            } else {
-                Log.w(getString(R.string.app_name), "XML Error: Incorrect CharSequence[] in " +
-                        "license_display read, value is " + Arrays.toString(infoArray));
-            }
+            licenseView.addLicense(infoArray);
         }
-        setContentView(returnScroll);
     }
 
     @Override
